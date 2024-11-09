173d4cc426bea14eccd9bca19531c740577694bf
rauf993 <rauf.hamden5056@alumnos.udg.mx>
2024-11-08 19:19:14 -0600

    1.0.0

--- App/React-Native-mysql/Backend/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-require("./database");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-_app["default"].listen(3000);
-console.log('listening on port 3000');
