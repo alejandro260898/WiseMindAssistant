161f9c31dc2ccc3ec8f2f0832c53ed638482bc02
rauf993 <rauf.hamden5056@alumnos.udg.mx>
2024-11-20 14:45:27 -0600

    supabase en web

--- WiseMindAssistant/connection/supabase.ts
+++ WiseMindAssistant/connection/supabase.ts
@@ -6,6 +6,6 @@ import * as SecureStore from 'expo-secure-store'
 
 //console.log(process.env.URL,process.env.API_KEY)
 //dconst supabaseURL='http://mystifying-rain-15393.pktriot.net/'
-const supabaseURL='http://192.168.100.20:8000'
-const supabaseKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE'
+const supabaseURL='https://abgspujwyujtccknqenr.supabase.co'
+const supabaseKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiZ3NwdWp3eXVqdGNja25xZW5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0MzU5NzgsImV4cCI6MjAyNjAxMTk3OH0.NOjPxUVPBYztUlLCl6CBYg9vIrl9I58zD6bolUzqYfs'
 export const supabase=createClient(supabaseURL,supabaseKey)
